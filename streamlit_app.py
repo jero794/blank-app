939243727f637d3dcdc2e783780331a386494655
jero794 <0242992@up.edu.mx>
2024-08-20 18:09:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,57 @@
-import streamlit as st
+## Jeronimo Ramirez Pujol
+Av. Vallarta Eje Poniente 2701, Los Sueños Residencial, 45222, Zapopan
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+(33) 1131 5020 jeronimo.ramirezprc@gmail.com
+
+### Personal Statement
+I am currently a student of the bachelor's degree in Finance and Administration. I am 
+deeply interested in developing my professional career in the financial sector, specifically
+in asset management. I am looking for a job where I can constantly learn new investment
+strategies, gain experience, and apply my knowledge.
+
+### Experience
+
+#### A&J Maquinas Expendedoras - 2022
+I was a co-founder of A&J, a company focused on comprehensive
+service with state-of-the-art vending machines. The main
+activities I performed included monitoring purchases made in
+the machines, inventory management, creating income
+statements for the machines, and overall financial statements
+for the company.
+
+#### Universidad Panamericana de Guadalajara - 2023
+I worked in the public relations field. My main activities included
+attending events both within and outside the city to promote the
+university. I gave promotional talks about the university and
+provided guidance to students on admission processes.
+
+#### CAPX - 2023
+I am a co-founder of CAPX, a company focused on creating
+profitability for its clients through digital marketing strategies.
+The main activities I perform include operations management,
+customer service oversight, and overall administration of the
+company.
+
+### Education
+
+#### Universidad Panamericana de Guadalajara
+Current student of the Bachelor’s Degree in Finance and
+Administration
+
+### Certificates
+
+Toefl ITP
+
+### Languages
+
+Spanish - Native Language
+English - Upper Advanced
+
+### Skills
+
+- Strong leadership and strategic planning skills
+- Excellent customer service skills
+- Proficient in Microsoft Office Suite (Proficiency in creating and automating macros)
+- Experience with marketing software
+- Strong analytical and problem-solving skills
+- Experience with accounting software
