64a2f1ab8395d46e27817db7b8d8b38501812e58
jero794 <0242992@up.edu.mx>
2024-08-20 18:22:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,4 +66,4 @@ st.sidebar.info("Soy un estudiante que se esta capacitando en Python")
 
 # También puedes agregar imágenes en la barra lateral
 st.sidebar.title("Mi empresa")
-st.sidebar.image("CAPX_Logo-02.png")
+st.sidebar.image("C:\Users\Jerónimo Ramírez\OneDrive - up.edu.mx\UP\7mo Semestre\Ing Fin.png")
